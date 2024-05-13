4efea6b4a06b3d711378e2edf1d2cd99e509f041
gunch1258 <gunch1258@yahoo.com>
2024-05-13 14:06:59 +0900

    Add Requests
    Updated

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -30,6 +30,9 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => ['required'],
+        ]);
         Category::create(['name' => $request->input('name')]);
 
         return redirect()->route('categories.index');
@@ -56,6 +59,9 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
+        $request->validate([
+            'name' => ['required'],
+        ]);
         $category->update(['name' => $request->input('name')]);
         return redirect()->route('categories.index')->with('success', '');
     }
