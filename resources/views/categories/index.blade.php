9aa5b3287212330b3f269a91b30bbab9dbb149d9
gunch1258 <gunch1258@yahoo.com>
2024-05-13 15:52:44 +0900

    Update

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
+        @vite('resources/css/app.css')
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Categories') }}
         </h2>
@@ -9,29 +10,37 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <a href="{{ route('categories.create') }}">Add New Category</a>
+                    <a href="{{ route('categories.create') }}"
+                        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Add
+                        New Category</a>
+
                     <table>
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($categories as $category)
-                                <tr>
-                                    <td>{{ $category->name }}</td>
-                                    <td><a href="{{ route('categories.edit', $category) }}">Edit</a>
-                                        <form method="POST" action="{{ route('categories.destroy', $category) }}">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-inverse-warning"
-                                                onclick="return confirm ('Are you sure you want to delete this category this will also delete the related posts?')">Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
+                        <div class="table-responsive">
+                            <table id="dataTableExample" class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($categories as $category)
+                                        <tr>
+                                            <td>{{ $category->name }}</td>
+                                            <td><a href="{{ route('categories.edit', $category) }}">Edit</a>
+                                                <form method="POST"
+                                                    action="{{ route('categories.destroy', $category) }}">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-inverse-warning"
+                                                        onclick="return confirm ('Are you sure you want to delete this category this will also delete the related posts?')">Delete</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </table>
                 </div>
             </div>
