fe08ff2d0ee1e2faccda07a9cb1a87d02d43fb6c
gunch1258 <gunch1258@yahoo.com>
2024-05-15 09:49:00 +0900

    update

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -39,7 +39,9 @@
                                 @endforeach
                             </select>
                         </div>
-                        <div>
+                        <div class="py-3">
+                            <a class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                                href="{{ route('posts.index') }}">Cancel</a>
                             <button type="submit"
                                 class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                                 Save
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,6 +19,7 @@
                     <a href="{{ route('posts.create') }}"
                         class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Add
                         New Post</a>
+
                     <table class="table">
                         <thead>
                             <tr>
@@ -45,6 +46,7 @@
                                 </tr>
                             @endforeach
                         </tbody>
+
                     </table>
                 </div>
             </div>
